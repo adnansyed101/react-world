89347b176b50fb35f50100f2525a860d9b652d7f
adnan <sirgeant10@gmail.com>
2024-10-19 22:20:30 +0600

    Show countries flag and name

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,4 +1,5 @@
-import { useState ,useEffect } from "react";
+import { useState, useEffect } from "react";
+import Country from "../Country/Country";
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
@@ -12,6 +13,9 @@ const Countries = () => {
   return (
     <div>
       <h3>Countries: {countries.length}</h3>
+      {countries.map((country) => (
+        <Country key={country.cca3} country={country}></Country>
+      ))}
     </div>
   );
 };
