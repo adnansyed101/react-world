6b15f18a150ced8ec888e29b9d2373fe323df7c2
adnan <sirgeant10@gmail.com>
2024-10-19 22:30:39 +0600

    Show population and area

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,14 @@
 import "./Country.css";
 
 const Country = ({ country }) => {
-  const { name, flags } = country;
+  const { name, flags, population, area } = country;
 
   return (
     <div className="country">
-        <img src={flags.png} alt="" />
+      <img src={flags.png} alt="" />
       <h3>Name: {name?.common}</h3>
+      <p>Population: {population}</p>
+      <p>Area: {area}</p>
     </div>
   );
 };
